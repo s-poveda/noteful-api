71638a2d8d19a4046564bf28f9dcdfecd42828b2
sam p <spoveda545@gmail.com>
2020-11-05 14:50:57 -0500

    knex setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
+const knex = require('knex');
 const app = require('./app.js');
-const { PORT } = require('./config');
+const { PORT, DATABSE_URL } = require('./config');
+
+app.set('db', knex({
+	client: 'pg',
+	connection: DATABSE_URL
+}));
 
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
